5b568b5a2b1178132fc5e0a4da84fac102253400
Lídia Torres <109477475+lilytorrx@users.noreply.github.com>
2025-05-08 11:56:41 -0300

    Herança e Polimorfismo

--- src/Conta.java
+++ src/Conta.java
@@ -2,8 +2,14 @@ public abstract class Conta implements ContaInterface {
     private int agencia;
     private int numeroConta;
     private double saldo;
+    private static int agenciaPadrao = 1; 
+    private static int sequencial = 0;
 
-    
+    public Conta() {
+        this.agencia = agenciaPadrao;
+        this.numeroConta = sequencial++;
+    }
+
     public int getAgencia() {
         return agencia;
     }
