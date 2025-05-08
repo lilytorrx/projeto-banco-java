ca12d2473cc95ee8a195d4c13e6287c421866416
Lídia Torres <109477475+lilytorrx@users.noreply.github.com>
2025-05-08 11:22:20 -0300

    Abstração do problema

---
+++ src/ContaPoupanca.java
@@ -0,0 +1,17 @@
+public class ContaPoupanca {
+    private int agencia;
+    private int numeroConta;
+    private double saldo;
+
+    public void sacar() {
+
+    }
+
+    public void depositar() {
+        
+    }
+    
+    public void transferir() {
+        
+    }
+}
